6ad911463d8c50d04ae8cf6808d1715833e2ee2c
Pawel <ipablo1981@icloud.com>
2020-03-03 00:59:09 +0000

    Aded file structure and webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require("path");
+const {
+    CleanWebpackPlugin
+} = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: "./app/assets/scripts/App.js",
+    output: {
+        filename: "js/[name].js",
+        path: path.resolve(__dirname, "build")
+    },
+    devServer: {
+        open: true,
+        contentBase: path.resolve(__dirname, "app/assets/images"),
+        port: 5001
+    },
+    mode: "development",
+    watch: true,
+    module: {
+        rules: [{
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.(scss|sass)$/,
+                use: ["style-loader", "css-loader", "sass-loader"]
+            },
+            {
+                test: /\.js$/,
+                loader: "babel-loader",
+                exclude: /node_modules/,
+                options: {
+                    presets: [
+                        [
+                            "@babel/preset-env",
+                            {
+                                useBuiltIns: "usage",
+                                corejs: "2.0.0"
+                            }
+                        ]
+                    ],
+                    plugins: ["@babel/plugin-proposal-class-properties"]
+                }
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: "app/templates/template.html",
+            title: "Design Agency"
+        })
+    ]
+};
---
+++ webpack.config.prod.js
@@ -0,0 +1,60 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+	entry: "./app/assets/scripts/App.js",
+	output: {
+		filename: "js/[name]-[contenthash].js",
+		path: path.resolve(__dirname, "build")
+	},
+	mode: "production",
+	watch: true,
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: [MiniCssExtractPlugin.loader, "css-loader"]
+			},
+			{
+				test: /\.(scss|sass)$/,
+				use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+			},
+			{
+				test: /\.js$/,
+				loader: "babel-loader",
+				exclude: /node_modules/,
+				options: {
+					presets: [
+						[
+							"@babel/preset-env",
+							{
+								useBuiltIns: "usage",
+								corejs: "2.0.0"
+							}
+						]
+					],
+					plugins: ["@babel/plugin-proposal-class-properties"]
+				}
+			}
+		]
+	},
+	plugins: [
+		new CleanWebpackPlugin(),
+		new HtmlWebpackPlugin({
+			template: "app/templates/template.html",
+			title: "Design Agency"
+		}),
+		new MiniCssExtractPlugin({
+			filename: "css/[name]-[contenthash].css"
+		}),
+		new CopyPlugin([
+			{
+				from: "app/assets/images",
+				to: "images"
+			}
+		])
+	]
+};
