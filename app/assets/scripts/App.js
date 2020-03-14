cc1c3d19164e62779f2e9e62c80a65e1c3c8274f
Pawel <ipablo1981@icloud.com>
2020-03-14 02:14:50 +0000

    Add styles and functionality to header

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,3 +1,10 @@
 // import "../scss/styles.scss";
 import "../scss/styles.scss";
 import "./MobileMenu";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+AOS.init({
+    offset: 200,
+    duration: 1000
+});
