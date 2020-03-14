7b9cb8b4a46ec829dfea61f74f36e428d0335f9f
Pawel <ipablo1981@icloud.com>
2020-03-14 03:55:32 +0000

    Complited lazy loading of images

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,10 +1,11 @@
 // import "../scss/styles.scss";
 import "../scss/styles.scss";
 import "./MobileMenu";
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import "lazysizes";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 AOS.init({
-    offset: 200,
-    duration: 1000
+	offset: 200,
+	duration: 1000
 });
